7237049cee297e5213f12fd88714b4b07583bdd6
Omprakash Sridharan <omprakash.sridharan@sumup.com>
2023-06-13 22:53:33 +0200

    Adds repository tests

--- books_api/src/repository/mod.rs
+++ books_api/src/repository/mod.rs
@@ -37,9 +37,11 @@ impl Repository {
 
 #[cfg(test)]
 mod tests {
+    use sea_orm::{DbErr, RuntimeErr};
+    use sea_orm::sea_query::error::Error;
     use testcontainers::{clients, images};
     use database::get_connection;
-    use crate::repository::Repository;
+    use crate::repository::{Repository, RepositoryError};
 
     #[tokio::test]
     async fn test_create_book() {
@@ -57,5 +59,26 @@ mod tests {
         let created_book = repository.create_book(title.clone(), isbn.clone()).await.unwrap();
         assert_eq!(created_book.title, title.clone());
         assert_eq!(created_book.isbn, isbn.clone());
+    }
+
+    #[tokio::test]
+    async fn test_create_book_title_unique() {
+        let docker = clients::Cli::default();
+        let database = images::postgres::Postgres::default();
+        let node = docker.run(database);
+        let connection_string = &format!(
+            "postgres://postgres:postgres@127.0.0.1:{}/postgres",
+            node.get_host_port_ipv4(5432)
+        );
+        let database_connection = get_connection(connection_string).await.unwrap();
+        let repository = Repository::new(database_connection.clone()).await.unwrap();
+        let title = "TITLE".to_string();
+        let isbn = "ISBN".to_string();
+        let created_book1 = repository.create_book(title.clone(), isbn.clone()).await.unwrap();
+        assert_eq!(created_book1.title, title.clone());
+        assert_eq!(created_book1.isbn, isbn.clone());
+
+        let created_book2_result = repository.create_book(title.clone(), isbn.clone()).await;
+        assert!(created_book2_result.is_err());
     }
 }
