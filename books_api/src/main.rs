2b23f7bb72ef271905015029cd19b198ad7a7bd8
Omprakash Sridharan <omprakash.sridharan@sumup.com>
2023-06-13 22:39:53 +0200

    Adds testcontainers

--- books_api/src/main.rs
+++ books_api/src/main.rs
@@ -1,19 +1,15 @@
 mod entity;
 mod repository;
 
-use std::sync::Arc;
 use database::get_connection;
-use migration::{Migrator, MigratorTrait};
-use crate::repository::RepositoryBuilder;
+use crate::repository::{Repository};
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
     let database_connection = get_connection("postgres://postgres:postgres@localhost/rust-superapp").await?;
-    Migrator::up(&database_connection, None).await?;
-
-    let repository = RepositoryBuilder::default().database_connection(Arc::new(database_connection.clone())).build().expect("Could not build repository");
+    let repository = Repository::new(database_connection.clone()).await.expect("Error creating repository");
 
     Ok(())
 }
--- books_api/src/repository/mod.rs
+++ books_api/src/repository/mod.rs
@@ -1,11 +1,11 @@
 use std::sync::Arc;
-use derive_builder::Builder;
 use sea_orm::{ActiveModelTrait, DatabaseConnection, DbErr};
 use sea_orm::ActiveValue::Set;
 use thiserror::Error;
+use migration::{Migrator, MigratorTrait};
 use crate::entity::book::{Model as BookModel, ActiveModel as BookActiveModel};
 
-#[derive(Builder)]
+
 pub struct Repository {
     database_connection: Arc<DatabaseConnection>,
 }
@@ -17,6 +17,13 @@ pub enum RepositoryError {
 }
 
 impl Repository {
+    pub async fn new(database_connection: DatabaseConnection) -> Result<Self, RepositoryError> {
+        Migrator::up(&database_connection, None).await.map_err(|e| RepositoryError::DatabaseError(e))?;
+        Ok(Self {
+            database_connection: Arc::new(database_connection)
+        })
+    }
+
     pub async fn create_book(&self, title: String, isbn: String) -> Result<BookModel, RepositoryError> {
         let created_book = BookActiveModel {
             title: Set(title),
@@ -24,5 +31,31 @@ impl Repository {
             ..Default::default()
         };
         created_book.insert(self.database_connection.as_ref()).await.map_err(|e| RepositoryError::DatabaseError(e))
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use testcontainers::{clients, images};
+    use database::get_connection;
+    use crate::repository::Repository;
+
+    #[tokio::test]
+    async fn test_create_book() {
+        let docker = clients::Cli::default();
+        let database = images::postgres::Postgres::default();
+        let node = docker.run(database);
+        let connection_string = &format!(
+            "postgres://postgres:postgres@127.0.0.1:{}/postgres",
+            node.get_host_port_ipv4(5432)
+        );
+        let database_connection = get_connection(connection_string).await.unwrap();
+        let repository = Repository::new(database_connection.clone()).await.unwrap();
+        let title = "TITLE".to_string();
+        let isbn = "ISBN".to_string();
+        let created_book = repository.create_book(title.clone(), isbn.clone()).await.unwrap();
+        assert_eq!(created_book.title, title.clone());
+        assert_eq!(created_book.isbn, isbn.clone());
     }
 }
