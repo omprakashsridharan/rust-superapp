b98f91f5e304115c6c8e6fb6e0197383c4264488
Omprakash Sridharan <omprakash.sridharan@sumup.com>
2023-06-18 21:14:27 +0200

    Fix tracing

--- books_api/src/main.rs
+++ books_api/src/main.rs
@@ -5,15 +5,32 @@ mod repository;
 mod service;
 
 use crate::repository::Repository;
-use common_tracing::setup_tracing;
 use database::get_connection;
 use http_server::start_http_server;
 use opentelemetry::global;
 use service::{book_created_producer::BookCreatedProducer, Service};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    setup_tracing().await;
+    opentelemetry::global::set_text_map_propagator(opentelemetry_zipkin::Propagator::new());
+    let tracer = opentelemetry_zipkin::new_pipeline()
+        .with_service_name("service".to_owned())
+        .with_service_address("127.0.0.1:8080".parse().unwrap())
+        .with_collector_endpoint("http://localhost:9411/api/v2/spans")
+        .install_batch(opentelemetry::runtime::Tokio)
+        .expect("unable to install zipkin tracer");
+    let tracer = tracing_opentelemetry::layer().with_tracer(tracer.clone());
+
+    let subscriber = tracing_subscriber::fmt::layer().json();
+
+    let level = EnvFilter::new("debug".to_owned());
+
+    tracing_subscriber::registry()
+        .with(subscriber)
+        .with(level)
+        .with(tracer)
+        .init();
     let database_connection =
         get_connection("postgres://postgres:postgres@localhost/rust-superapp").await?;
     let repository = Repository::new(database_connection.clone())
